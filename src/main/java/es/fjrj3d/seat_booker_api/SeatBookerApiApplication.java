d03ccb5f36763114e5fc1e538b59637702316e0b
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-10-14 19:40:15 +0200

    first commit

---
+++ src/main/java/es/fjrj3d/seat_booker_api/SeatBookerApiApplication.java
@@ -0,0 +1,13 @@
+package es.fjrj3d.seat_booker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatBookerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatBookerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/fjrj3d/seat_booker_api/SeatBookerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.fjrj3d.seat_booker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatBookerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
