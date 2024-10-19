300f002d2f7c026dfb9f06ce4aed894609628718
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-10-19 16:05:12 +0200

    Added CRUD operations for Room entity

--- src/main/java/es/fjrj3d/seat_booker_api/services/RoomService.java
+++ src/main/java/es/fjrj3d/seat_booker_api/services/RoomService.java
@@ -1,12 +1,37 @@
 package es.fjrj3d.seat_booker_api.services;
 
+import es.fjrj3d.seat_booker_api.models.Room;
 import es.fjrj3d.seat_booker_api.repositories.IRoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoomService {
 
     @Autowired
     private IRoomRepository iRoomRepository;
+
+    public Room createMovie(Room room){
+        return iRoomRepository.save(room);
+    }
+
+    public List<Room> getAllMovies(){
+        return iRoomRepository.findAll();
+    }
+
+    public Optional<Room> getMovieById(Long id){
+        return iRoomRepository.findById(id);
+    }
+
+    public Room updateMovie(Room room, Long id){
+        room.setId(id);
+        return iRoomRepository.save(room);
+    }
+
+    public void deleteMovie(Long id){
+        iRoomRepository.deleteById(id);
+    }
 }
