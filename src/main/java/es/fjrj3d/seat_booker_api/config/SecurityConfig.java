81b7fc38269b7f3ea93d12127d466cb9ce3b615e
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-10-29 12:21:33 +0100

    Modify SecurityConfig and application.properties for better authentication management

--- src/main/java/es/fjrj3d/seat_booker_api/config/SecurityConfig.java
+++ src/main/java/es/fjrj3d/seat_booker_api/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
-                        req.requestMatchers("/api/auth/**")
+                        req.requestMatchers("/api/auth/**", "/seat-booker-websocket")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
