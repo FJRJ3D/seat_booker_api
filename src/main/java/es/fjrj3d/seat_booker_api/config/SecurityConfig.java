1eeecb8efa7acd62a23fe81c4bc73e8055b47133
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2025-02-19 12:06:23 +0100

    Update SecurityCongig

--- src/main/java/es/fjrj3d/seat_booker_api/config/SecurityConfig.java
+++ src/main/java/es/fjrj3d/seat_booker_api/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
-                        req.requestMatchers("/api/auth/**", "/seat-booker-websocket", "/ai/generate", "/ai/generateStream").permitAll()
+                        req.requestMatchers("/api/auth/**", "/seat-booker-websocket", "/ai/generateStream").permitAll()
                                 .requestMatchers("/api/user/{newRole}/**").hasAuthority("ADMIN")
                                 .anyRequest()
                                 .authenticated()
