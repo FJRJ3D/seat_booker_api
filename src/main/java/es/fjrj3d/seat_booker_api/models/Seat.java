8001dbabe05585931183265055eadc22e1c48fab
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2025-03-04 11:12:44 +0100

    Minor update in Seat model.

--- src/main/java/es/fjrj3d/seat_booker_api/models/Seat.java
+++ src/main/java/es/fjrj3d/seat_booker_api/models/Seat.java
@@ -1,7 +1,6 @@
 package es.fjrj3d.seat_booker_api.models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
